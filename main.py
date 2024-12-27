c7b8459a72eb12825bedab47ff5c9c376addd5e6
Shyam <141533475+Shyam-duba@users.noreply.github.com>
2024-12-27 10:21:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -137,5 +137,8 @@ def extract_pdf_text_to_html(pdf_path):
 # Example usage
 
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
+
